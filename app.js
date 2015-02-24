c02720bed62f32ff77d3cf58cd79f578d1ad8e35
ewertonorg <ewertonorg@gmail.com>
2015-02-24 00:14:53 -0300

    segunda aula mean stack

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes');
 
@@ -7,11 +8,19 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// adicionando o middleware body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/',routes.index);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', process.env.PORT || 3000);
+app.get('/', routes.index);
+app.get('/lista', routes.lista);
+app.post('/grava', routes.grava);
+app.set('port', process.env.port || 3000);
 
-var server = app.listen(app.get('port'), function(){
-	console.log('Servidor foi startado na porta ' + server.address().port);
+var server = app.listen(app.get('port'), function() {
+    console.log('Servidor foi startado na porta ' + server.address().port);
 });
